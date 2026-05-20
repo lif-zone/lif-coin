1c269a498f618eddadde38f656f20f7709cd0447
Bright Life <bright@bright.center>
2026-05-21 02:05:28 +0300

    update

--- browser/wallet.jsx
+++ browser/wallet.jsx
@@ -623,7 +623,7 @@ function Wallet_screen({wallet, onDelete, onUpdate, onSelectTx,
         }
       </div>
       <div style={{display: 'flex', gap: 8, marginTop: 14, flexWrap: 'wrap', alignItems: 'center'}}>
-        <button onClick={onReceive} disabled={!allAddrs.length}>Receive</button>
+        <button onClick={onReceive}>Receive</button>
         <button onClick={onSend} disabled={!allAddrs.length}>Send</button>
         {netconf.lif_kv && <button onClick={onKvAdd} disabled={!allAddrs.length}>Get Domain Name</button>}
         {netconf.lif_kv && settings.ls.devtools && <button onClick={onKvAddRaw} disabled={!allAddrs.length}>Get Key/Val</button>}
