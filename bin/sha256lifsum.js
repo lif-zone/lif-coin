992d8966ece46c408addf6844330062a39c24e81
Bright Life <bright@bright.center>
2026-07-21 01:54:43 +0300

    update

--- bin/sha256lifsum.js
+++ bin/sha256lifsum.js
@@ -1,10 +1,24 @@
 #!/usr/bin/env node
 'use strict';
+const SHA256 = require('../lib/utils/sha256');
 const SHA256LIF = require('../lib/utils/sha256lif');
+const arg = process.argv.find(a=>a=='sha256'||a=='sha256lif'||a=='hash256'||a=='hash256lif') || 'sha256lif';
 const chunks = [];
 process.stdin.on('data', chunk=>chunks.push(chunk));
 process.stdin.on('end', ()=>{
   const buf = Buffer.concat(chunks);
-  const hash = SHA256LIF.digest(buf);
+  let hash;
+  if (arg=='sha256')
+    hash = SHA256.digest(buf);
+  else if (arg=='sha256lif')
+    hash = SHA256LIF.digest(buf);
+  else if (arg=='hash256')
+    hash = SHA256.digest(SHA256.digest(buf));
+  else if (arg=='hash256lif')
+    hash = SHA256LIF.digest(SHA256.digest(buf));
+  else {
+    console.log('inknown sha');
+    process.exit(1);
+  }
   process.stdout.write(hash.toString('hex')+'\n');
 });
