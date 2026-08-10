93c9c63b9289a40b04f8f7dad0cc7375d43a3d53
Bright Life <bright@bright.center>
2026-08-10 22:17:51 +0300

    update

--- install/install_bin.sh
+++ install/install_bin.sh
@@ -2,7 +2,7 @@
 
 cd ~/lif-coin/install
 
-BIN_FILES="ffind g gitdiff gitup rgrep start-coin-lifnet.sh start-coin.sh start-kernel-prod.sh start-kernel.sh tmux2.sh tmux.sh lif_reload_service.sh"
+BIN_FILES="ffind g gitdiff gitup rgrep start-coin-lifnet.sh start-coin.sh start-kernel-prod.sh start-kernel.sh tmux2.sh tmux.sh lif_reload_service.sh lif_stop_service.sh"
 for i in $BIN_FILES; do
   cp ./$i ~/bin/
 done
---
+++ install/lif_stop_service.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+sudo systemctl stop lif-kernel
+sudo systemctl stop lif-coin
+sudo systemctl stop lif-coin-lifnet
+
