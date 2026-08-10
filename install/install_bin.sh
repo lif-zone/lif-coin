95700e2d2341b118f7282436f7e0c4812b7864ea
Bright Life <bright@bright.center>
2026-08-10 17:23:04 +0300

    update

--- install/install_bin.sh
+++ install/install_bin.sh
@@ -1,3 +1,9 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
-cp ~/lif-coin/install/* ~/bin/
+cd ~/lif-coin/install
+
+BIN_FILES="ffind g gitdiff gitup rgrep start-coin-lifnet.sh start-coin.sh start-kernel-prod.sh start-kernel.sh tmux2.sh tmux.sh"
+for i in $BIN_FILES; do
+  cp ./$i ~/bin/
+done
+sudo cp ./lif-kernel.service /etc/systemd/system/
