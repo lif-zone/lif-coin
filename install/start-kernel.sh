39ac084227b2105f9093910d4c62c93df832323d
Bright Life <bright@bright.center>
2026-08-11 23:53:54 +0300

    update

--- install/start-kernel.sh
+++ install/start-kernel.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
 cd ~/lif-wallet
 git pull
 cd ~/lif-kernel
 rm package-lock.json
 git pull
 npm install
-sudo npm run serve-prod 
-# sudo node ./web/server.js -s
+#sudo npm run serve-prod 
+CMD='node ./web/server.js -s'
+sudo capsh --caps="cap_net_bind_service,cap_setuid,cap_setgid,cap_setpcap+eip" --user="$USER" --addamb=cap_net_bind_service -- -c "$CMD"
+
