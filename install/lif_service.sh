54d074f2f2746a74b002d736ee375ea6bd88de40
Bright Life <bright@bright.center>
2026-08-14 13:52:18 +0300

    added lif-explorer

--- install/lif_service.sh
+++ install/lif_service.sh
@@ -8,10 +8,13 @@ if [ "$1" == start ] ; then
   sudo systemctl status lif-coin
   sudo systemctl start lif-coin-lifnet
   sudo systemctl status lif-coin-lifnet
+  sudo systemctl start lif-explorer
+  sudo systemctl status lif-explorer
 elif [ "$1" == stop ] ; then
   sudo systemctl stop lif-kernel || true
   sudo systemctl stop lif-coin || true
   sudo systemctl stop lif-coin-lifnet || true
+  sudo systemctl stop lif-explorer || true
 elif [ "$1" == restart ] ; then
   sudo systemctl daemon-reload
   sudo systemctl restart lif-kernel
@@ -20,14 +23,18 @@ elif [ "$1" == restart ] ; then
   sudo systemctl status lif-coin
   sudo systemctl restart lif-coin-lifnet
   sudo systemctl status lif-coin-lifnet
+  sudo systemctl restart lif-explorer
+  sudo systemctl status lif-explorer
 elif [ "$1" == status ] ; then
   sudo systemctl status lif-kernel || true
   sudo systemctl status lif-coin || true
   sudo systemctl status lif-coin-lifnet || true
+  sudo systemctl status lif-explorer || true
 elif [ "$1" == enable ] ; then
   sudo systemctl enable lif-kernel
   sudo systemctl enable lif-coin
   sudo systemctl enable lif-coin-lifnet
+  sudo systemctl enable lif-explorer
 elif [ "$1" == disable ] ; then
   sudo systemctl stop lif-kernel || true
   sudo systemctl disable lif-kernel
@@ -35,10 +42,13 @@ elif [ "$1" == disable ] ; then
   sudo systemctl disable lif-coin
   sudo systemctl stop lif-coin-lifnet || true
   sudo systemctl disable lif-coin-lifnet
+  sudo systemctl stop lif-explorer || true
+  sudo systemctl disable lif-explorer
 elif [ "$1" == is-enabled ] ; then
   sudo systemctl is-enabled lif-kernel || true
   sudo systemctl is-enabled lif-coin || true
   sudo systemctl is-enabled lif-coin-lifnet || true
+  sudo systemctl is-enabled lif-explorer || true
 else
   echo "lif_service.sh start|stop|restart|status|enable|disable|is-enabled"
 fi
