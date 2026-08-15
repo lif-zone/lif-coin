96d19195df04fc6f470093bc0eb5590970d0f3d1
LIF Deploy <lifdeploy@lif.zone>
2026-08-15 21:01:52 +0300

    update

--- scripts/gen_genesis.js
+++ scripts/gen_genesis.js
@@ -72,9 +72,9 @@ function createGenesisBlock(opt) {
     flags = Buffer.from(flags, 'ascii');
   if (!key) {
     key = buf_from_hex(''
-      + '04678afdb0fe5548271967f1a67130b7105cd6a828e039'
-      + '09a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c3'
-      + '84df7ba0b8d578a4c702b6bf11d5f');
+      +'04678afdb0fe5548271967f1a67130b7105cd6a828e039'
+      +'09a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c3'
+      +'84df7ba0b8d578a4c702b6bf11d5f');
   }
   if (!reward)
     reward = 50 * consensus.COIN;
